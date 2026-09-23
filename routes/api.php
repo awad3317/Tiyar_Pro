dd59f91e3eb3b87a481201906fd643c0b99e94ae
awad3317 <127610663+awad3317@users.noreply.github.com>
2026-09-23 13:47:54 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ParcelBotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::post('/whatsapp/webhook', [ParcelBotController::class, 'handleWebhook']);
