39ccd296bc3d705930132e99d3cb3d57a3195489
awad3317 <127610663+awad3317@users.noreply.github.com>
2026-09-24 08:57:48 +0300

    Update ParcelBotController.php

--- app/Http/Controllers/ParcelBotController.php
+++ app/Http/Controllers/ParcelBotController.php
@@ -313,7 +313,7 @@ class ParcelBotController extends Controller
 
             // في حال واجه أي بطء نقوم بتجربة الموديل البديل المتاح في حسابك
             if (!$response->successful()) {
-                $fallbackUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-lite-latest:generateContent?key={$apiKey}";
+                $fallbackUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={$apiKey}";
                 $response = Http::withHeaders([
                     'Content-Type' => 'application/json',
                 ])->timeout(40)->post($fallbackUrl, [
